ee9909408f81a5754b0a02455db10a8be6c11865
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-06-03 19:39:56 +0300

    Create register reactive form

--- src/Client/src/app/components/auth/auth.component.ts
+++ src/Client/src/app/components/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent {
+  isRightPanelActive: boolean = false;
 
+  registerForm = this.fb.group({
+    username: ['', Validators.required, Validators.minLength(3), Validators.maxLength(20)],
+    email: ['', Validators.required, Validators.email],
+    password: ['', Validators.required, Validators.minLength(6)]
+  });
+
+  constructor(private fb: FormBuilder) { }
+
+  register() {
+    console.log('register');
+  }
+
+  changePanelActivity() {
+    this.isRightPanelActive = !this.isRightPanelActive;
+  }
 }
