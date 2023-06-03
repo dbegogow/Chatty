dcd3c2db54b0dcf3bcd61cd817d0414f310b0bdb
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-06-03 18:26:00 +0300

    Create Auth component and register a route for it

--- src/Client/src/app/app-routing.module.ts
+++ src/Client/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './components/auth/auth.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: AuthComponent },
+  { path: '**', redirectTo: 'login' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
