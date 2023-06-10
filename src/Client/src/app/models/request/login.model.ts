203d07dd294f084e7a5879186b86c8ea6de82ebc
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-06-10 14:40:55 +0300

    Implement login auth service model

---
+++ src/Client/src/app/models/request/login.model.ts
@@ -0,0 +1,4 @@
+export interface LoginModel {
+    email: string | null | undefined;
+    password: string | null | undefined;
+}
--- src/Client/src/app/services/auth.service.ts
+++ src/Client/src/app/services/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { RegisterModel } from '../models/request/register.model';
+import { LoginModel } from '../models/request/login.model';
 import { IdentityModel } from '../models/response/identity.model';
 
 @Injectable({
@@ -13,5 +14,9 @@ export class AuthService {
 
   register(model: RegisterModel): Observable<IdentityModel> {
     return this.http.post<IdentityModel>(`${environment.apiUrl}/api/register`, model);
+  }
+
+  login(model: LoginModel): Observable<IdentityModel> {
+    return this.http.post<IdentityModel>(`${environment.apiUrl}/api/login`, model);
   }
 }
