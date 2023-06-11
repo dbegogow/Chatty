1436a7e55200931f141e765968183810298e72d7
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-06-11 11:30:46 +0300

    Make when open the chat to be scrolled at the bottom

--- src/Client/src/app/components/chats/chats.component.ts
+++ src/Client/src/app/components/chats/chats.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-chats',
   templateUrl: './chats.component.html',
   styleUrls: ['./chats.component.css']
 })
-export class ChatsComponent {
+export class ChatsComponent implements OnInit {
+  @ViewChild('scrollableMessages', { static: true }) scrollableElementRef!: ElementRef;
 
+
+  ngOnInit() {
+    this.messagesScrollToBottom();
+  }
+
+  messagesScrollToBottom() {
+    const scrollableElement = this.scrollableElementRef.nativeElement;
+    scrollableElement.scrollTop = scrollableElement.scrollHeight;
+  }
 }
